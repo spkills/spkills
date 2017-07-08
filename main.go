5f91cfb4e2c7b037e355349ae6fd4b97c5ef62d1
Ryo Manzoku <manzoku.ryo@gmail.com>
2017-07-08 16:10:06 +0900

    Use UNIX socket
    たぶんこれがいちばんはやいとおもいます

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/spkills/spkills/controller"
 )
@@ -11,7 +16,32 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	listenPort := "8080"
-	http.HandleFunc("/", rootHandler)
-	http.ListenAndServe(":"+listenPort, nil)
+	socketFile := "/spkills/run/spkills.sock"
+	mux := http.NewServeMux()
+
+	// Routers
+	mux.HandleFunc("/", rootHandler)
+
+	ln, err := net.Listen("unix", socketFile)
+	if err != nil {
+		log.Fatalf("error %v", err)
+	}
+
+	defer func(ln net.Listener) {
+		if err := ln.Close(); err != nil {
+			log.Fatalf("error %v", err)
+		}
+	}(ln)
+
+	c := make(chan os.Signal, 2)
+	go func(ln net.Listener, c chan os.Signal) {
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		<-c
+		if err := ln.Close(); err != nil {
+			log.Fatalf("error %v", err)
+		}
+		os.Exit(1)
+	}(ln, c)
+
+	log.Fatalf("error %v", http.Serve(ln, mux))
 }
