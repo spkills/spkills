a01f2f7fb1f34ff6a9e00cdba923a4ad38d15b3d
mogge <muzigen@gmail.com>
2017-07-08 17:48:23 +0900

    templateをhtmlに

--- view/view.go
+++ view/view.go
@@ -1,11 +1,12 @@
 package view
 
 import (
-	"fmt"
 	"net/http"
 	"github.com/spkills/spkills/templates"
 )
 
 func RootView(w http.ResponseWriter, str string) {
-	fmt.Fprintf(w, templates.Hello("Foo"))
+	p := &templates.MainPage{}
+	templates.WritePageTemplate(w,p)
+	//fmt.Fprintf(w, templates.Hello(str))
 }
