ae3a5b18f57f9881daf90f5a580fe3a4f8df8365
ayumu83s <ayumu.itou.1125@gmail.com>
2017-09-24 14:00:13 +0900

    generate db models (#25)
    * add sqlboiler
    
    * add sqlboiler config
    
    * go generateでモデルを自動生成
    
    * sqlboilerが生成するディレクトリを変更
    
    * schemaディレクトリは破棄
    
    * パラメータをファイルに押し込める

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 var conf config.Config
 
 //go:generate cmd/ready/ready -file=route.conf
+//go:generate sqlboiler mysql
 func main() {
 	_, err := toml.DecodeFile("config/config.toml", &conf)
 	if err != nil {
