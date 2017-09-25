ce7ca555005351360679f99734a394d5f5937bdd
ayumu83s <ayumu.itou.1125@gmail.com>
2017-09-25 23:06:14 +0900

    テストをちょっと修正

--- model/cache_test.go
+++ model/cache_test.go
@@ -18,14 +18,14 @@ func TestInitCache(t *testing.T) {
 func TestFetchByCache(t *testing.T) {
 	InitCache()
 	closureA := func(id int) interface{} {
-		return 0
+		return id
 	}
 	closureB := func(id int) interface{} {
 		return "piyo"
 	}
 	t.Run("キャッシュがなければ関数の値", func(t *testing.T) {
 		result := FetchByCache("hoge", 10*time.Second, closureA, 1)
-		if result.(int) != 0 {
+		if result.(int) != 1 {
 			t.Fatalf("closureA not called")
 		}
 	})
