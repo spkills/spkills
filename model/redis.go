888815b928a1d55bb849063f9cf0a6ae0287eba1
ayumu83s <ayumu.itou.1125@gmail.com>
2017-10-01 16:52:49 +0900

    redisもsocketで接続

--- model/redis.go
+++ model/redis.go
@@ -3,6 +3,8 @@ package model
 import (
 	"fmt"
 	"log"
+	"net"
+	"os"
 
 	"github.com/go-redis/redis"
 	"github.com/spkills/spkills/config"
@@ -11,13 +13,33 @@ import (
 var redisClient *redis.Client
 
 func InitRedis(conf config.Config) {
+	var addr string
+	var network string
+	var dialer func() (net.Conn, error)
+	_, err := os.Stat(conf.Database.SocketFile)
+	if err == nil {
+		network = "unix"
+		dialer = func() (net.Conn, error) {
+			conn, err := net.Dial("unix", conf.Redis.SocketFile)
+			if err != nil {
+				log.Fatalf("error: %v", err)
+			}
+			return conn, err
+		}
+	} else {
+		network = "tcp"
+		addr = fmt.Sprintf("%s:%s", conf.Redis.Server, conf.Redis.Port)
+	}
+
 	redisClient = redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("%s:%d", conf.Redis.Server, conf.Redis.Port),
+		Addr:     addr,
 		Password: conf.Redis.Password,
 		DB:       conf.Redis.DB,
+		Network:  network,
+		Dialer:   dialer,
 	})
-	_, err := redisClient.Ping().Result()
-	if err != nil {
-		log.Panic(err)
-	}
+	// _, err := redisClient.Ping().Result()
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
 }
