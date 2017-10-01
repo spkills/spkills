77713b368bd5c89f8f5a347ceb1c765a874d74fc
ayumu83s <ayumu.itou.1125@gmail.com>
2017-10-01 17:12:28 +0900

    socketfileの指定でよかった

--- model/redis.go
+++ model/redis.go
@@ -2,8 +2,6 @@ package model
 
 import (
 	"fmt"
-	"log"
-	"net"
 	"os"
 
 	"github.com/go-redis/redis"
@@ -15,17 +13,10 @@ var redisClient *redis.Client
 func InitRedis(conf config.Config) {
 	var addr string
 	var network string
-	var dialer func() (net.Conn, error)
 	_, err := os.Stat(conf.Database.SocketFile)
 	if err == nil {
 		network = "unix"
-		dialer = func() (net.Conn, error) {
-			conn, err := net.Dial("unix", conf.Redis.SocketFile)
-			if err != nil {
-				log.Fatalf("error: %v", err)
-			}
-			return conn, err
-		}
+		addr = conf.Redis.SocketFile
 	} else {
 		network = "tcp"
 		addr = fmt.Sprintf("%s:%s", conf.Redis.Server, conf.Redis.Port)
@@ -36,7 +27,6 @@ func InitRedis(conf config.Config) {
 		Password: conf.Redis.Password,
 		DB:       conf.Redis.DB,
 		Network:  network,
-		Dialer:   dialer,
 	})
 	// _, err := redisClient.Ping().Result()
 	// if err != nil {
