9e2151a3c12b2e5ea45bfe2bc261709de99d3d70
Ryo Manzoku <manzoku.ryo@gmail.com>
2017-10-06 18:43:33 +0900

    Update

--- cmd/ready/controller/routing.go
+++ cmd/ready/controller/routing.go
@@ -4,9 +4,25 @@ import (
 	"github.com/buaazp/fasthttprouter"
 )
 
-func Routing(router *fasthttprouter.Router) {
+// AddRouteing add routing path from go generate ready
+func AddRouting(router *fasthttprouter.Router) {
 
-	router.GET("/test", testHandler)
-	router.GET("/test_1", test_1Handler)
-	router.GET("/test_2", test_2Handler)
+	router.GET("/test", TestGetHandler)
+	router.POST("/test", TestPostHandler)
+	router.PUT("/test", TestPutHandler)
+	router.DELETE("/test", TestDeleteHandler)
+	router.HEAD("/test", TestHeadHandler)
+
+	router.GET("/test_1", Test_1GetHandler)
+	router.POST("/test_1", Test_1PostHandler)
+	router.PUT("/test_1", Test_1PutHandler)
+	router.DELETE("/test_1", Test_1DeleteHandler)
+	router.HEAD("/test_1", Test_1HeadHandler)
+
+	router.GET("/test_2", Test_2GetHandler)
+	router.POST("/test_2", Test_2PostHandler)
+	router.PUT("/test_2", Test_2PutHandler)
+	router.DELETE("/test_2", Test_2DeleteHandler)
+	router.HEAD("/test_2", Test_2HeadHandler)
+
 }
